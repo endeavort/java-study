cbcbc6a324d8e14a7ce93510cd5fceacefe7c1cb
220100 <220100kd@yse-c.net>
2023-06-02 10:09:08 +0900

    文章の出力

---
+++ sample/src/MessageApp.java
@@ -0,0 +1,15 @@
+public class MessageApp {
+
+    // public:全てのクラスからアクセス可能
+    // static:インスタンス化せずに使用可能
+    // void:戻り値なし
+    // main:プログラム本文
+    // String[] args:コマンド引数
+    public static void main(String[] args) {
+        // println:出力の終わりに改行（sysoで予測出てくる）
+        System.out.println("こんにちは！");
+        // print:出力の終わりに改行しない
+        System.out.print("私の名前は");
+        System.out.print("佐藤です");
+    }
+}
