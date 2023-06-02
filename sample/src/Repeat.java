73c7cdf7e699777aba68215082ee5e78459aa4ff
220100 <220100kd@yse-c.net>
2023-06-02 12:02:24 +0900

    while文

--- sample/src/Repeat.java
+++ sample/src/Repeat.java
@@ -20,13 +20,39 @@ public class Repeat {
         String[] drinks = { "コーヒー", "紅茶", "ほうじ茶" };
         // .length:要素数の取得
         for (int i = 0; i < drinks.length; i++) {
-            System.out.println(drinks[i]);
+            // System.out.println(drinks[i]);
         }
 
         // foreach:インデックスなしで配列の要素を取得
         for (String drink : drinks) {
-            System.out.println(drink);
+            // System.out.println(drink);
         }
+
+        // while
+        int currentNumber = 1;
+        int maxNumber = 1000;
+        int div = 7;
+        int index = 0;
+        int limit = 5;
+        // currentNumber < maxNumberがTrueの間ずっと繰り返す
+        while (currentNumber < maxNumber) {
+            if (currentNumber % div == 0) {
+                // System.out.println(currentNumber);
+                index++;
+            }
+            // break:ループから抜ける（index == limit のときループから抜ける）
+            // if (index == limit) {
+            // break;
+            // }
+            currentNumber++;
+        }
+
+        // do while:処理をした後に条件を判定
+        int i = 0;
+        do {
+            i++;
+        } while (i <= 10);
+        // System.out.println("whileの結果:" + i);
     }
 
 }
