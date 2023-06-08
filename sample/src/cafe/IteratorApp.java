4a0b02888a3d7dbdcc374d0703695aaaf1a9735e
220100 <220100kd@yse-c.net>
2023-06-08 09:55:20 +0900

    マップのイテレーター

--- sample/src/cafe/IteratorApp.java
+++ sample/src/cafe/IteratorApp.java
@@ -1,7 +1,10 @@
 package cafe;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class IteratorApp {
     public static void main(String[] args) {
@@ -20,6 +23,53 @@ public class IteratorApp {
             Drink drink = iterator.next(); // 次の要素を取得
             System.out.println(drink.name);
         }
+
+        // マップ作成
+        Map<Integer, Drink> drinkMap = new HashMap<>();
+
+        // マップに要素の追加
+        drinkMap.put(1, new Drink("コーヒー", 350));
+        drinkMap.put(2, new Drink("紅茶", 400));
+        drinkMap.put(3, new Drink("ほうじ茶", 300));
+
+        // キーを取得してイテレーター作成
+        Iterator<Integer> mapIterator = drinkMap.keySet().iterator();
+        while (mapIterator.hasNext()) {
+            Integer key = mapIterator.next();
+            System.out.println(key);
+        }
+
+        // 値を取得してイテレーター作成
+        Iterator<Drink> valueIterator = drinkMap.values().iterator();
+        while (valueIterator.hasNext()) {
+            Drink drink = valueIterator.next();
+            System.out.println("name:" + drink.name + " price:" + drink.price);
+        }
+
+        // iterator:片方向に処理
+        // ListIterator:両方向から処理できる（逆方向からもできる）
+
+        // イテレーター作成
+        ListIterator<Drink> listIterator = drinkList.listIterator();
+
+        // add:next()によって返される要素(ある場合)の直前、およびprevious()によって返される要素(ある場合)の直後に挿入される。
+        listIterator.add(new Drink("カフェモカ", 350));
+
+        // 先ほど追加したカフェモカは表示されない
+        // while (listIterator.hasNext()) {
+        // Drink drink = listIterator.next();
+        // System.out.println(drink.name);
+        // }
+
+        // previous:1つ前に戻す
+        listIterator.previous();
+
+        // 先ほど追加したカフェモカが表示される
+        while (listIterator.hasNext()) {
+            Drink drink = listIterator.next();
+            System.out.println(drink.name);
+        }
+
     }
 
 }
